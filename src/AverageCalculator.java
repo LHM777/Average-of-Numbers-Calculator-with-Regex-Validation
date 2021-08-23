c18af7a1cafb30329afd3d5529924d8287e17910
LHM777 <777harvey777@gmail.com>
2021-08-23 17:11:54 +0800

    AverageCalculator.java

--- src/AverageCalculator.java
+++ src/AverageCalculator.java
@@ -1,1 +1,13 @@
+package com.lhm777.averagecalculator;
 
+
+public class AverageCalculator {
+    
+    
+    public static void main(String args[]){
+    
+        AverageCalculatorGUI x =   new AverageCalculatorGUI();
+        x.setMinimumSize(x.getMinimumSize());
+        x.setVisible(true);
+    }
+}
